97cade74adb3511ee2d6630c24b7aa147a5df580
Kevin Alvarado <kevin.alvarado8502@utc.edu.ec>
2021-05-04 18:16:48 -0500

    Subiendo a herokus

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 import dj_database_url
 from decouple import config
-DATABASES{
+DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
