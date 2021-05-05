92a247549e5b82dbb9f4dda12c9c1f69ad6dbe28
Kevin Alvarado <kevin.alvarado8502@utc.edu.ec>
2021-05-04 21:28:14 -0500

    REALIZADO

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hwx)nobmmrixp1+)m^2+fe%t@dqo9xx*f3$jenh%-$19rk^_#8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://mejiaproradio.herokuapp.com/']
 #ALLOWED_HOSTS = ['192.168.0.111']
 
 
